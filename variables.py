7c0e4f39e72332baa038e99779ffe488ba4c8e93
rahuldhirendersingh <rahuldhirendersingh@gmail.com>
2023-08-24 15:26:20 +0530

    variable in python

---
+++ variables.py
@@ -0,0 +1,32 @@
+# variable is like a container where you can store your data.
+
+# to create a variable you need to give your variable a name then use an assignment operator, and then assign the value.
+
+
+a = 10
+b = 15
+
+print(a + b)
+print(a * b)
+
+name = "Rahul"
+print(name)
+
+firstName = "John "
+lastName = "Smith"
+
+print(firstName + lastName) # string concatenation.
+
+a = 20
+print(a) # the value of the variable could be changed in future.
+
+
+# RULES TO NAME IDENTIFIERS :-
+
+'''
+- identifier can contain alphabet characters, digits, underscore.
+- identifier should not start with a digit.
+- identifier are case sensitive.
+- we cannot use reserved words as identifiers.
+- special symbols like dollar ($) symbol is not allowed in python.
+'''
