fe6831eeae6b454fb67a11592ebac6309b5dd7a3
rahuldhirendersingh <rahuldhirendersingh@gmail.com>
2023-08-24 17:53:20 +0530

    built in functions in python

---
+++ built-in-functions.py
@@ -0,0 +1,35 @@
+# Built in Functions -> Python has a set of built-in functions.
+
+# input() - is used to take user input.
+
+name = input("enter your name")
+
+age = input("enter your age?") # input function always take input in str form.
+
+# eval() - evaluate the value of a string and convert it into it's actual data type.
+
+a = eval('3' + '5')
+print(a)
+
+b = eval('9 + 2')
+print(b)
+
+print(type(a))
+print(type(b))
+
+# print is also a built in function used to display outputs.
+
+print(456)
+print('rahul singh')
+
+# min() & max()
+
+print(min(5,8,9,16,3))
+print(max(5,8,9,16,3))
+
+
+# -eval-
+
+x = eval("856")
+
+print(type(x))
