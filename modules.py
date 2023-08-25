720258ca42b4b66ac381234326a2bd692513d3f9
rahuldhirendersingh <rahuldhirendersingh@gmail.com>
2023-08-25 19:25:27 +0530

    built in modules

---
+++ modules.py
@@ -0,0 +1,32 @@
+# MODULES
+
+# RANDOM MODULE
+
+import random
+
+print(random.random())
+
+print(random.random())
+
+print(int(random.random() * 6 + 1)) # make a dice which return value between 1 to 6.
+
+
+
+# MATH MODULE
+
+import math as m
+
+print(m.ceil(12.14))
+print(m.floor(12.14))
+print(m.fabs(-121))
+print(m.pow(12, 3))
+print(m.exp(4))
+print(m.log(12, 7))
+print(m.log10(2))
+print(m.sqrt(100))
+print(m.sin(m.pi/6))
+print(m.cos(m.pi/4))
+print(m.tan(m.pi/3))
+print(m.pi)
+print(m.degrees(m.pi/2))
+print(m.radians(m.pi/6))
